301189f0bd6db38275210cc9d977f69807c5ac6a
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2025-11-21 15:22:22 +0530

    feat: implement TradingView widget and refactor navigation items

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,27 @@
+import TradingViewWidget from "@/components/TradingViewWidget";
+import {
+  HEATMAP_WIDGET_CONFIG,
+  MARKET_DATA_WIDGET_CONFIG,
+  MARKET_OVERVIEW_WIDGET_CONFIG,
+  TOP_STORIES_WIDGET_CONFIG,
+} from "@/lib/constants";
 const Home = () => {
-  return <div className="flex min-h-screen home-wrapper">Home</div>;
+  const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
+  return (
+    <div className="flex min-h-screen home-wrapper">
+      <section className="grid w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            title="Market Overview"
+            scriptUrl={`${scriptUrl}market-overview.js`}
+            config={MARKET_OVERVIEW_WIDGET_CONFIG}
+            className="custom-chart"
+            height={600}
+          />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
