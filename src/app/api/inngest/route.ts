69d9a87873eeddf5e92a9605a896751ce8366bbf
Venkata Rajesh Jakka <rajesh.jakka1@gmail.com>
2025-12-23 20:23:25 +0530

    Daily news

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,8 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { sendSignUpEmail } from "@/lib/inngest/function";
+import { sendDailyNewsSummary, sendSignUpEmail } from "@/lib/inngest/function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [sendSignUpEmail],
+  functions: [sendSignUpEmail, sendDailyNewsSummary],
 });
